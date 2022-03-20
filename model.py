c1b832bb9dba195904fe524ba9f6662b10aa367b
douglasfraser01 <101813241+douglasfraser01@users.noreply.github.com>
2022-03-20 11:06:34 -0400

    Update model.py

--- model.py
+++ model.py
@@ -23,6 +23,15 @@ print(training_images[random_index])
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+
+
+
+
 model.compile(optimizer = tf.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
